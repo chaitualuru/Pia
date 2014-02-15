41b80380bc02b342fa62924772bfbfb6930befcd
Krishna Chaitanya Aluru <Chaitu@Chaitanyas-Mac.local>
2014-02-15 03:24:28 -0500

    complete remodel

---
+++ js/background_scripts.js
@@ -0,0 +1,44 @@
+document.body.style.backgroundColor = 'red';
+
+var stylesheet = document.createElement('link');
+
+var div = document.createElement('div');
+var microphone_div = document.createElement('div');
+var result_div = document.createElement('div');
+var info_div = document.createElement('div');
+
+var microphone_script = document.createElement('script');
+var control_script = document.createElement('script');
+
+stylesheet.rel = 'stylesheet';
+stylesheet.href = 'http://athyuttamreddy.com/hosted/microphone.css';
+document.head.appendChild(stylesheet);
+
+document.body.appendChild(div);
+div.id = 'myDivId';
+div.style.position = 'fixed';
+div.style.backgroundColor = 'blue';
+div.style.top = '10px';
+div.style.right = '10px';
+div.style.width = '500px';
+div.style.height = '500px';
+
+microphone_div.id = 'microphone';
+result_div.id = 'result';
+info_div.id = 'info';
+
+div.appendChild(microphone_div);
+div.appendChild(result_div);
+div.appendChild(info_div);
+
+microphone_script.src = 'http://athyuttamreddy.com/hosted/microphone.js';
+control_script.src = 'http://athyuttamreddy.com/hosted/control.js';
+
+document.body.appendChild(microphone_script);
+document.body.appendChild(control_script);
+
+// iframe = document.createElement('iframe');
+// iframe.src="http://rtcshare.appspot.com";
+// iframe.width = '960px';
+// iframe.height = '1200px';
+// document.body.appendChild(iframe);
---
+++ js/control.js
@@ -0,0 +1,44 @@
+var mic = new Wit.Microphone(document.getElementById("microphone"));
+    var info = function (msg) {
+      document.getElementById("info").innerHTML = msg;
+    };
+    info("Adding random text!");
+    mic.onready = function () {
+      info("Microphone is ready to record");
+    };
+    mic.onaudiostart = function () {
+      info("Recording started");
+    };
+    mic.onaudioend = function () {
+      info("Recording stopped, processing started");
+    };
+    mic.onerror = function (err) {
+      info("Error: " + err);
+    };
+    mic.onresult = function (intent, entities) {
+      var r = kv("intent", intent);
+
+      for (var k in entities) {
+        var e = entities[k];
+
+        if (!(e instanceof Array)) {
+          r += kv(k, e.value);
+        } else {
+          for (var i = 0; i < e.length; i++) {
+            r += kv(k, e[i].value);
+          }
+        }
+      }
+
+      document.getElementById("result").innerHTML = r;
+    };
+    mic.connect("JZMUMU653TE5XIZKKYCCX4MCAWDFZAWH");
+    // mic.start();
+    // mic.stop();
+
+    function kv (k, v) {
+      if (toString.call(v) !== "[object String]") {
+        v = JSON.stringify(v);
+      }
+      return k + "=" + v + "\n";
+    }
