034be76254c63d595bdb30ff1b73a62e127309c2
Krishna Chaitanya Aluru <Chaitu@Chaitanyas-Mac.local>
2014-02-16 06:58:46 -0500

    all changes

--- js/layout.js
+++ js/layout.js
@@ -1,6 +1,8 @@
 if ($("#pia_container").length > 0) {
   	console.log("Pia exists. Now removing.");
-  	$("#pia_container").remove();
+  	$("#pia_container").animate({right:"30px"},200).animate({right:"-300px"},200, function() {
+  		$("#pia_container").remove();
+  	});
 }
 else {
 	console.log("Pia doesn't exist. Now creating.")
@@ -11,17 +13,25 @@ else {
 
 	var div = document.createElement('div');
 	var microphone_div = document.createElement('div');
+	var text_div = document.createElement('div');
 	var result_div = document.createElement('div');
 	var info_div = document.createElement('div');
+	var sound_container = document.createElement('span');
 
 	document.body.appendChild(div);
 	div.id = 'pia_container';
 
+	$("#pia_container").animate({right:"-300px"},0).animate({right:"25px"},200).animate({right:"10px"},200);
+
+	text_div.id = 'pia_text_div';
 	microphone_div.id = 'pia_microphone';
 	result_div.id = 'pia_result';
 	info_div.id = 'pia_info';
+	sound_container.id = 'sound_container';
 
 	div.appendChild(microphone_div);
-	div.appendChild(result_div);
-	div.appendChild(info_div);
+	div.appendChild(text_div);
+	div.appendChild(sound_container);
+	text_div.appendChild(result_div);
+	text_div.appendChild(info_div);
 }
